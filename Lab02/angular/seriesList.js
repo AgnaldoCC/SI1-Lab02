436d9f8ef13b6be5b31ee737504ccd1898ffd219
AgnaldoCC <agnaldo.junior@ccc.ufcg.edu.br>
2017-06-12 12:29:49 -0300

    Botões 'Minhas Series' adicionados

--- Lab02/angular/seriesList.js
+++ Lab02/angular/seriesList.js
@@ -4,6 +4,8 @@ app.controller("seriesController", function($scope, $http){
   $scope.pesquisa = "Pesquisar Séries";
 
   $scope.series = [];
+  $scope.minhasSeries = [];
+  $scope.watchList = []
 
   $scope.getSeries = function(nome){
 		var promise = $http.get('http://www.omdbapi.com/?s=' + nome + '&type=series&apikey=93330d3c').then(function(response){
@@ -14,18 +16,27 @@ app.controller("seriesController", function($scope, $http){
 		return promise;
 	};
 
-  $scope.minhasSeries = [];
-
   $scope.adicionaSerie = function(serie){
     $scope.minhasSeries.push(serie);
   };
-
   $scope.contains = function(serie){
     for (var i = 0; i < minhasSeries.length; i++) {
       if (minhasSeries[i].Title == serie.Title){
+        
         return true;
       }
     }return false;
   };
+
+  $scope.deletarSerie = function(serie){
+    var index = $scope.minhasSeries.indexOf(serie);
+    if (index > -1){
+      $scope.minhasSeries.splice(index, 1);
+    }
+  }
+
+  $scope.adicionarWatchlist = function(serie){
+    $scope.watchList.push(serie);
+  }
 
 });
